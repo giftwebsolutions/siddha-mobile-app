3b66c1835b5c8f6c0ef6faa2b2b441e66bb0e7c0
Prabhu <giftwebsolutions@gmail.com>
2025-03-20 10:53:04 +0530

    product list component

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { ProductComponent } from './product.component';
 
-
+const routes: Routes = [
+  {
+    path: 'list',
+    component: ListComponent
+  },
+  {
+    path: ':id',
+    component: ProductComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ProductModule { }
