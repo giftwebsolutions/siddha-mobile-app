aa3500b1da974a98295830fe4acea90b9f708491
Prabhu <giftwebsolutions@gmail.com>
2025-04-18 17:04:53 +0530

    icon and splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.giftweb.siddhamedicals',
   appName: 'siddha-medicals',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      backgroundColor: '#ffffff',
+      androidSplashResourceName: 'splash',
+      iosSplashResourceName: 'splash',
+      showSpinner: false,
+    },
+  },
 };
 
 export default config;
